e7fd881b7a3a116ca868f726d774112bca895484
dikaizm <dikamaah@gmail.com>
2023-03-12 16:03:30 +0700

    update contents

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import FileExtensionValidator
 
 # Create your models here.
+class Client:
+    id: int
+    name: str
+    img: str
+
+class Service(models.Model):
+    name = models.CharField(max_length=30)
+    details = models.CharField(max_length=400)
+    img = models.FileField(upload_to='static/images/icons/services/', validators=[FileExtensionValidator(['jpg', 'jpeg', 'png', 'svg'])])
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Client
+from .models import Service
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    client1 = Client()
+    client1.id = 0
+    client1.name = 'Ideax'
+    client1.img = '/static/images/icons/clients/ideax.svg'
+
+    client2 = Client()
+    client2.id = 1
+    client2.name = 'Alfamart'
+    client2.img = '/static/images/icons/clients/alfamart.svg'
+
+    client3 = Client()
+    client3.id = 2
+    client3.name = 'Filantropi'
+    client3.img = '/static/images/icons/clients/filantropi.svg'
+
+    clients = [client1, client2, client3]
+
+    services = Service.objects.all()
+
+    return render(request, 'index.html', {'clients': clients, 'services': services})
