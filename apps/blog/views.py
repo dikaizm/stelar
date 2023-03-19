28b4e10b55d6cdf8ca8862444e64b5561f173f43
dikaizm <dikamaah@gmail.com>
2023-03-19 20:50:47 +0700

    update database

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -4,11 +4,11 @@ from .models import Post
 # Create your views here.
 def blog(request):
     posts = Post.objects.all()
-
     return render(request, 'blog.html', {'posts': posts})
 
 def post(request):
-    return render(request, 'post.html')
+    posts = Post.objects.all()
+    return render(request, 'post.html', {'posts': posts})
 
 def author(request):
     return render(request, 'author.html')
